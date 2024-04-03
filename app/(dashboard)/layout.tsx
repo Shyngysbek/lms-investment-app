2ef5930842d6c136f95193b482652018cc2929b2
Shyngysbek <bbalgabay.shyngysbek@gmail.com>
2024-04-03 15:49:32 +0200

    Add: Chapter HLS Video Player

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ const DashboardLayout = ({
 }) => {
   return ( 
     <div className="h-full">
-      <div className="md:pl-56 h-[80px] fixed inset-y-0 w-full">
+      <div className="md:pl-56 h-[80px] fixed inset-y-0 w-full z-50">
         <Navbar />
       </div>
       <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
